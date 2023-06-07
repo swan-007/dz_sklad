2ba533814b82a478437f4f058cd482410166b028
Sergey Lebedev <superlebedew@gmail.com>
2023-06-07 15:47:29 +0300

    cicd

--- stocks_products/logistic/views.py
+++ stocks_products/logistic/views.py
@@ -1,9 +1,11 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Product, Stock
+from .models import Stock, Product
 from .serializers import ProductSerializer, StockSerializer
-from django_filters.rest_framework import DjangoFilterBackend
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
@@ -20,3 +22,9 @@ class StockViewSet(ModelViewSet):
     serializer_class = StockSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['products']
+
+@api_view(['GET'])
+def sample_view(request):
+    text = '''Этот текст предназначен для проверки того,
+    что мы сделали всё правильно'''
+    return Response(text)
